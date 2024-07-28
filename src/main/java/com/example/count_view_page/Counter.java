0ee9fec6e0087a6bd3f31b0bdbc5e27152d4a0f4
tinhnguyen269 <tinhnguyen180507@gmail.com>
2024-07-28 08:29:04 +0700

    thuc hanh

---
+++ src/main/java/com/example/count_view_page/Counter.java
@@ -0,0 +1,24 @@
+package com.example.count_view_page;
+
+public class Counter {
+    private int count;
+
+    public Counter() {
+    }
+
+    public Counter(int count) {
+        this.count = count;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public void increment() {
+        count++;
+    }
+}
---
+++ src/test/java/com/example/count_view_page/CountViewPageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.count_view_page;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountViewPageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
